c6eefe38fa5d3523b179d180bdee1fbd77102d30
luis <luislopez0690@gmail.com>
2018-02-14 17:45:06 -0500

    added info-navbar scss

--- app/components/nav-bars/info-navbar.js
+++ app/components/nav-bars/info-navbar.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   elementId: 'info-navbar',
-  classNames: ['d-flex justify-content-between']
+  classNames: ['container']
 });
