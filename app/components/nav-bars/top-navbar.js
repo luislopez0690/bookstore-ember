9edb02619700396101fc776f6b61ab7d043c9cb9
luis <luislopez0690@gmail.com>
2018-02-15 14:39:24 -0500

    Top-navbar stylind done

--- app/components/nav-bars/top-navbar.js
+++ app/components/nav-bars/top-navbar.js
@@ -3,5 +3,5 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'nav',
   elementId: 'top-navbar',
-  classNames: ['navbar navbar-expand-lg navbar-dark bg-dark']
+  classNames: ['navbar navbar-expand-lg']
 });
