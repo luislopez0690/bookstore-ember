f26a988fa6889e8178db031dcc6fd803459251db
luis <luislopez0690@gmail.com>
2018-02-20 11:49:02 -0500

    first take on ember modals, need to add information for buying

--- app/components/all-books.js
+++ app/components/all-books.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    toggleModal: function() {
+      this.toggleProperty('isShowingModal');
+    }
+  }
 });
